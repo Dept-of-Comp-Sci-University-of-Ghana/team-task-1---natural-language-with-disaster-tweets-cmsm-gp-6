9d62fca6b288f617d778e5d3314d91ca97f7d750
Francis Anderson <wherlberg198@gmail.com>
2023-07-09 17:47:45 +0000

    initial commit

---
+++ .github/model.py
@@ -0,0 +1,4 @@
+#building a machine learning model that predicts which Tweets are about real disasters and which ones aren’t 
+#from the data sets using natural llanguage processing
+
+#import necessary packages
